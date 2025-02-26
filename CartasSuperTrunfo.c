6e1666a9bc020cf7811f517d3a714235b55821ba
GuiFBrito <75683002+GuiFBrito@users.noreply.github.com>
2025-02-26 18:13:40 +0000

    programa pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,5 +21,72 @@ int main() {
 
     printf("Jogo de trunfos\n");
 
+
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos1, pontos2;
+
+        printf("jogo de supertrunfo - cadastro de cartas\n");
+        printf("voce ira cadastrar duas cartas\n");
+
+        //cadastro da carta 01
+
+        printf("carta 01:\n");
+
+        printf("população:");
+        scanf(" %d\n", &populacao1);
+
+        printf("area:");
+        scanf(" %f\n", &area1);
+
+        printf("pib:");
+        scanf(" %f\n", &pib1);
+
+        printf("pontos turisticos:");
+        scanf(" %d\n", &pontos1);
+
+        printf("carta 01 cadastrada com sucesso!!\n");
+        printf("carta01:\n");
+        printf("populacao: %d\n", populacao1);
+        printf("area: %f\n", area1);
+        printf("pib:%f\n", pib1);
+        printf("pontos turisticos: %d\n", pontos1);
+
+
+        //cadastro da segunda carta
+
+        printf("carta 02:\n");
+
+        printf("população:");
+        scanf(" %d\n", &populacao2);
+
+        printf("area:");
+        scanf(" %f\n", &area2);
+
+        printf("pib:");
+        scanf(" %f\n", &pib2);
+
+        printf("pontos turisticos:");
+        scanf(" %d\n", &pontos2);
+
+        printf("carta 02 cadastrada com sucesso!!\n");
+        printf("carta02:\n");
+        printf("populacao: %d\n", populacao2);
+        printf("area: %f\n", area2);
+        printf("pib:%f\n", pib2);
+        printf("pontos turisticos: %d\n", pontos2);
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
